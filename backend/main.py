d92dce6168b9f1bd3f4f380ccddd6f853e437fd3
AlyanAsif06 <alyanasif2717@gmail.com>
2026-01-20 23:58:42 +0500

    Made main.py for backend (middle layer b//w front and backend using FASTAPI)

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,26 @@
-from database import setup_db
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from schema import search_cars
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            #do this when url ready for our website allow_origins=["http://localhost:3000"]
+                                    #currnetly it allows all websites to call api
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/search")
+def search(
+    brand: str = Query(...),
+    name: str = Query(...),
+    price: int = Query(...),
+    condition: str = Query(...)
+):
+    results = search_cars(brand, name, price, condition)
+    return {"results": results}
