aeb404b582bb7a073322c181a03f1bcf34801bac
waleedhkirmani <waleedhassankirmani@gmail.com>
2026-01-20 22:14:18 +0500

    cards updated

--- frontend/src/Cards/react/Card.jsx
+++ frontend/src/Cards/react/Card.jsx
@@ -7,7 +7,7 @@ function Card(){
     return (
         <div id="card">
             <Image url="https://cache2.pakwheels.com/ad_pictures/1190/honda-vezel-hybrid-z-honda-sensing-2019-119065443.webp"/> 
-            <Info carName="Honda Civic" price="2300000" condition="A+"/> 
+            <Info carName="Bonda Vezel" price="2300000" condition="A+"/> 
             <Button url="https://www.pakwheels.com/used-cars/honda-vezel-2019-for-sale-in-lahore-9918177"/>
         </div>
     )
