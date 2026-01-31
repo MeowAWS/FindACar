6c8d24d2389a9bf3286aee95f9d8b47a5bbc0309
AlyanAsif06 <alyanasif2717@gmail.com>
2026-01-31 15:51:40 +0500

    design updated (pkwhls logo and more)

--- frontend/src/TopBar/TopBar.jsx
+++ frontend/src/TopBar/TopBar.jsx
@@ -1,15 +1,23 @@
 import "./TopBar.css"
-import logo from "../../assets/logo1.jpeg"
+import logo from "../../assets/logoTopBar.png"
+import leftLogo from "../../assets/pakwheels.png"
 
-function TopBar () {
+function TopBar() {
     return (
         <div id="topbar">
+
+
+            <div id="left-container">
+                <img src={leftLogo} alt="PakWheel" id="pakwheels" />
+                <span id="left-text">An Extension of PakWheels</span>
+            </div>
+
             <div id="logo-container">
                 <img src={logo} alt="Logo" id="logo" />
                 <p id="findacar">Find a Car</p>
             </div>
 
-            <a href="mailto:msubhannoor24.07@gmail.com">Contact Us</a>
+            <a href="mailto:msubhannoor24.07@gmail.com" id="contactUs">Contact Us</a>
         </div>
     )
 }
